d4558eb739dc8ebed4de2d8cee3e9516878fe5e2
m6xcom <maxcom.ukr@gmail.com>
2022-09-17 19:02:47 +0300

    destructures props

--- src/components/Field.js
+++ src/components/Field.js
@@ -2,25 +2,25 @@ import React , {useState, useEffect} from 'react'
 import {FieldItem} from "./FieldItem";
 import '../assets/scss/components/Field.scss'
 
-export function Field(props){
+export function Field({mode , setHoveredSquares, hoveredSquares, state}){
     const [squareArr, setSquareArr] = useState([]);
 
     useEffect(()=>{
         const tempArr = [];
-        for(let i = 0; i < props.mode ; i++){
+        for(let i = 0; i < mode ; i++){
             tempArr.push(i + 1);
         }
         setSquareArr(tempArr);
-    }, [props.mode]);
+    }, [mode]);
 
     return (
-        <div className={`field ${props.mode === 5 ? 'field--small' : props.mode === 15 ? 'field--medium' : 'field--big'}`}>
+        <div className={`field ${mode === 5 ? 'field--small' : mode === 15 ? 'field--medium' : 'field--big'}`}>
             {squareArr.map((row)=>{
                 return <div key={row} className='field__row'>
                     { squareArr.map((col)=>
-                        <FieldItem setHoveredSquares={props.setHoveredSquares}
-                                   hoveredSquares={props.hoveredSquares}
-                                   state={props.state} key={`${col}${row}`} col={col} row={row}/>) }
+                        <FieldItem setHoveredSquares={setHoveredSquares}
+                                   hoveredSquares={hoveredSquares}
+                                   state={state} key={`${col}${row}`} col={col} row={row}/>) }
                 </div>
             })}
         </div>
--- src/components/Select.js
+++ src/components/Select.js
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react'
 import {fetchModes} from "../services/modesApiService";
 import '../assets/scss/components/Select.scss'
 
-export function Select(props){
+export function Select({changeMode}){
     const [modes , setModes] = useState([]);
     const getAndSetModes = async()=>{
         try {
@@ -18,7 +18,7 @@ export function Select(props){
         if (select.value && select.classList.contains("select--error")){
             select.classList.remove('select--error');
         }
-        props.changeMode(e.currentTarget.value);
+        changeMode(e.currentTarget.value);
     }
     useEffect(()=>{
         getAndSetModes();
